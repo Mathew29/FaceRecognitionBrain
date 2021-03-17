b02a5428295d390cff8636dd3191837eb1574a4a
Mathew <mwakre29@gmail.com>
2021-03-17 16:34:24 -0700

    import logo img

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Tilt from "react-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt
-        className="Tilt"
-        options={{ max: 25 }}
-        style={{ height: 250, width: 250 }}
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner pa3">
+          <img src={brain} alt="brain" />{" "}
+        </div>
       </Tilt>
     </div>
   );
