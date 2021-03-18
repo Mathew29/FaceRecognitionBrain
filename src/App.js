805e6501f26a1629196716117baddb364b6fdcb5
Mathew <mwakre29@gmail.com>
2021-03-18 14:52:59 -0700

    add clarifai

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import "./App.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const app = new Clarifai.App({
+  apiKey: API_KEY,
+});
 
 const particlesOptions = {
   particles: {
@@ -30,6 +36,19 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log("click");
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
   };
 
   render() {
