3481fa104b9f80143e85dd9722b6298198d41d33
Mathew <mwakre29@gmail.com>
2021-03-18 13:57:59 -0700

    create on button submit handler

--- src/App.js
+++ src/App.js
@@ -25,7 +25,11 @@ class App extends Component {
   };
 
   onInputChange = (event) => {
-    console.log(event);
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
   };
 
   render() {
@@ -35,7 +39,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
